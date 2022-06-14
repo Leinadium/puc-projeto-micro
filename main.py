179c26a6aab6f6d11ab7bf68a7f004662868ce05
m.v.araujo.almeida@gmail.com <Amiablegem192>
2022-06-13 23:38:29 -0300

    Adicionando a interface gráfica

---
+++ main.py
@@ -0,0 +1,143 @@
+from time import sleep
+import pygame
+from pygame.locals import *
+from sys import exit
+from nota import Nota
+
+
+pygame.init()
+
+LARGURA = 720
+ALTURA = 480
+
+
+# CORES
+LARANJA = (231, 145, 17)
+AZUL = (42, 61, 229)
+AMARELO = (231, 196, 15)
+VERMELHO = (193, 27, 27)
+VERDE = (26, 153, 61)
+PRETO = (0, 0, 0)
+BRANCO = (255, 255, 255)
+map_cores = {
+    "azul": AZUL,
+    "vermelho": VERMELHO,
+    "amarelo": AMARELO,
+    "verde": VERDE,
+    "laranja": LARANJA,
+}
+
+
+RAIO_ACORDE = 15
+RAIO_ACORDE_MOVEL = 10
+TAMANHO_LINHA_VERTICAL = 439
+
+pos_cordas = [276.07, 320.24, 364.41, 408.57, 452.74]
+
+notas_tela = [Nota("verde", 0, 330, 0), Nota("vermelho", 1, 110, 0), Nota("verde", 0, 200, 0), Nota("laranja", 4, 150, 0)]
+
+tela = pygame.display.set_mode((LARGURA, ALTURA))
+pygame.display.set_caption("Guitar hero 3.5")
+tela.fill(BRANCO)
+pygame.display.flip()
+
+
+def desenha_acorde(cor, posX, posY):
+    pygame.draw.circle(tela, PRETO, (posX, posY), RAIO_ACORDE + 3)
+    pygame.draw.circle(tela, map_cores[cor], (posX, posY), RAIO_ACORDE)
+
+
+def desenha_acorde_movel(cor, posX, posY):
+    pygame.draw.circle(tela, PRETO, (posX, posY), RAIO_ACORDE_MOVEL + 3)
+    pygame.draw.circle(tela, map_cores[cor], (posX, posY), RAIO_ACORDE_MOVEL)
+
+
+def desenha_caminho_notas():
+    # caminho
+    s = pygame.Surface((229, 553))
+    s.set_alpha(70)
+    s.fill((160, 184, 231))
+    tela.blit(s, (248, 0))
+
+
+    # linhas verticais
+    pygame.draw.line(tela, PRETO, (pos_cordas[0], 0), (pos_cordas[0], TAMANHO_LINHA_VERTICAL), width=3)
+    pygame.draw.line(tela, PRETO, (pos_cordas[1], 0), (pos_cordas[1], TAMANHO_LINHA_VERTICAL), width=3)
+    pygame.draw.line(tela, PRETO, (pos_cordas[2], 0), (pos_cordas[2], TAMANHO_LINHA_VERTICAL), width=3)
+    pygame.draw.line(tela, PRETO, (pos_cordas[3], 0), (pos_cordas[3], TAMANHO_LINHA_VERTICAL), width=3)
+    pygame.draw.line(tela, PRETO, (pos_cordas[4], 0), (pos_cordas[4], TAMANHO_LINHA_VERTICAL), width=3)
+
+
+    # fim das notas
+    desenha_acorde("verde", pos_cordas[0], 450)
+    desenha_acorde("vermelho", pos_cordas[1], 450)
+    desenha_acorde("amarelo", pos_cordas[2], 450)
+    desenha_acorde("azul", pos_cordas[3], 450)
+    desenha_acorde("laranja", pos_cordas[4], 450)
+
+
+def desenha_pontuacao(pontos):
+    # caixa dos pontos
+    s = pygame.Surface((171, 105))
+    s.set_alpha(89)
+    s.fill((176, 176, 176))
+    tela.blit(s, (38, 38))
+
+
+    # texto dos pontos
+
+
+def desenha_tela():
+    tela.fill(BRANCO)
+    desenha_caminho_notas()
+
+
+def move_notas():
+    for nota in notas_tela:
+        nota.pos_y += 20
+
+
+def exibe_notas():
+    for nota in notas_tela:
+        desenha_acorde_movel(nota.cor, pos_cordas[nota.corda], nota.pos_y)
+    pygame.display.flip()
+
+
+def insere_nota_tela(nota):
+    notas_tela.append(nota)
+
+
+def remove_nota_tela(cor):
+
+    maior_y = -1
+    index_maior_y = -1
+
+    for (i, nota) in enumerate(notas_tela):
+        if cor == nota.cor:
+            if nota.pos_y > maior_y:
+                maior_y = nota.pos_y
+                index_maior_y = i
+
+    notas_tela.pop(index_maior_y)
+
+def proximo_segundo():
+    desenha_tela()
+    move_notas()
+    exibe_notas()
+
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            run = False
+            pygame.quit()
+
+    proximo_segundo()
+    sleep(1)
+
+
+
+
+if __name__ == "__main__":
+    print("OK")
---
+++ nota.py
@@ -0,0 +1,8 @@
+class Nota:
+
+    def __init__(self, cor, corda, pos_y, tempo_acorde):
+        self.cor = cor
+        self.corda = corda
+        self.pos_y = pos_y
+        self.tempo_acorde = tempo_acorde
+
